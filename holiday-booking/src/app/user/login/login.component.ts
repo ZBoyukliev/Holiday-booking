df3169e818f08d88a43d9490518a706ad876400c
ZBoyukliev <zarkoboyukliev@gmail.com>
2023-08-07 10:18:27 +0300

    login works correctly

--- holiday-booking/src/app/user/login/login.component.ts
+++ holiday-booking/src/app/user/login/login.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { UserService } from '../user.service';
 import { Router } from '@angular/router';
+import { ApiService } from 'src/app/api.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,14 +10,41 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-  // constructor(private userService: UserService, private router: Router) {}
-  email: string = ''; 
-  username: string = '';
-  password: string = ''; 
-  loginError: boolean = false;
+  @ViewChild('loginForm') loginForm: NgForm | undefined;
 
-  constructor(private userService: UserService) { }
+  constructor(
+    private apiService: ApiService,
+    private userService: UserService,
+    private router: Router
+  ) {}
 
- 
+  onLoginSubmitHandler(): void {
+    if (!this.loginForm) return;
+
+    const form = this.loginForm;
+
+    if (form.invalid) {
+      return;
+    }
+
+    const value: { email: string; password: string } = form.value;
+
+    this.apiService.login(value.email, value.password).subscribe({
+      next: (response) => {
+        if (response.accessToken) {
+          // this.apiService.clearToken();
+          this.userService.isLoggedIn = true;
+          this.apiService.setToken('accessToken', response.accessToken);
+
+          console.log('Login successful!');
+          this.router.navigate(['/']);
+          this.userService.showMessage('Logged in successfully!');          
+        }
+      },
+      error: (error) => {
+        this.userService.showMessage(error.error.message);        
+      },
+    });
+  }
 }
 
