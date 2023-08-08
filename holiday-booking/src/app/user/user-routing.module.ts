202d92a32be20b7abbf41f955e6ea3585c2966af
ZBoyukliev <zarkoboyukliev@gmail.com>
2023-08-08 14:51:50 +0300

    create hotel module and hotel service

--- holiday-booking/src/app/user/user-routing.module.ts
+++ holiday-booking/src/app/user/user-routing.module.ts
@@ -19,27 +19,3 @@ const routes: Routes = [
   exports: [RouterModule],
 })
 export class UserRoutingModule {}
-
-
-
-// import { NgModule } from '@angular/core';
-// import { RouterModule, Routes } from '@angular/router';
-// import { LoginComponent } from './login/login.component';
-// import { RegisterComponent } from './register/register.component';
-
-// const routes: Routes = [
-//   {
-//     path: 'login',
-//     component: LoginComponent,
-//   },
-//   {
-//     path: 'register',
-//     component: RegisterComponent,
-//   },
-// ];
-
-// @NgModule({
-//   imports: [RouterModule.forChild(routes)],
-//   exports: [RouterModule],
-// })
-// export class UserRoutingModule {}
