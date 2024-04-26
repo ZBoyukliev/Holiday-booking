e575b6bdc1abe47472c31a336f052e00a1bb2679
Zarko Boyukliev <122691228+ZBoyukliev@users.noreply.github.com>
2024-04-26 12:52:03 +0300

    Update api.service.ts

--- holiday-booking/src/app/api.service.ts
+++ holiday-booking/src/app/api.service.ts
@@ -2,7 +2,6 @@ import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 
-
 @Injectable({
     providedIn: 'root',
 })
