5d56f9ec8a16ea50589be4e9a99386f7a1e82fde
Zarko Boyukliev <122691228+ZBoyukliev@users.noreply.github.com>
2025-02-25 22:14:01 +0200

    Update authGuard.ts

--- holiday-booking/src/app/guards/authGuard.ts
+++ holiday-booking/src/app/guards/authGuard.ts
@@ -9,7 +9,6 @@ import { UserService } from "../user/user.service";
   export class AuthGuard implements CanActivate {
   
     constructor(private userService: UserService, private router: Router) { }
-  
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
       
       if (this.userService.isAuthenticated()) {
