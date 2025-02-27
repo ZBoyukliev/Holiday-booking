a4ef97b8c9c8c778edbda07fc8e824811e91faff
Zarko Boyukliev <122691228+ZBoyukliev@users.noreply.github.com>
2025-02-27 11:10:49 +0200

    Update register.component.spec.ts

--- holiday-booking/src/app/user/register/register.component.spec.ts
+++ holiday-booking/src/app/user/register/register.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RegisterComponent } from './register.component';
 
 describe('RegisterComponent', () => {
+  
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
 
@@ -13,7 +14,6 @@ describe('RegisterComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
   it('should create', () => {
     expect(component).toBeTruthy();
   });
