3c29b647289182350eee45edacfd7702d8f84c9d
Zarko Boyukliev <122691228+ZBoyukliev@users.noreply.github.com>
2025-03-06 21:49:55 +0200

    Update footer.component.spec.ts

--- holiday-booking/src/app/footer/footer.component.spec.ts
+++ holiday-booking/src/app/footer/footer.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FooterComponent } from './footer.component';
 
 describe('FooterComponent', () => {
+  
   let component: FooterComponent;
   let fixture: ComponentFixture<FooterComponent>;
 
