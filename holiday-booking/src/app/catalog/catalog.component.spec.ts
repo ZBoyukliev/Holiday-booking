1589ab89369e60278550fbcade4936d4eaf8f746
Zarko Boyukliev <122691228+ZBoyukliev@users.noreply.github.com>
2025-03-19 18:04:37 +0200

    Update catalog.component.spec.ts

--- holiday-booking/src/app/catalog/catalog.component.spec.ts
+++ holiday-booking/src/app/catalog/catalog.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CatalogComponent } from './catalog.component';
 
 describe('CatalogComponent', () => {
+  
   let component: CatalogComponent;
   let fixture: ComponentFixture<CatalogComponent>;
 
