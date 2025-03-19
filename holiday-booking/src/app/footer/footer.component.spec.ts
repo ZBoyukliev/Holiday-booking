8882e4ce2febe366b5a5e4caae530bda60bbc704
Zarko Boyukliev <122691228+ZBoyukliev@users.noreply.github.com>
2025-03-19 18:05:14 +0200

    Update footer.component.spec.ts

--- holiday-booking/src/app/footer/footer.component.spec.ts
+++ holiday-booking/src/app/footer/footer.component.spec.ts
@@ -10,12 +10,10 @@ describe('FooterComponent', () => {
     TestBed.configureTestingModule({
       declarations: [FooterComponent]
     });
-    
     fixture = TestBed.createComponent(FooterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
   it('should create', () => {
     expect(component).toBeTruthy();
   });
