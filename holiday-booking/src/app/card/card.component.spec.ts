aada6298180480cf2c642eabb7fce28e7e684e96
Zarko Boyukliev <122691228+ZBoyukliev@users.noreply.github.com>
2025-07-27 21:49:54 +0300

    Update card.component.spec.ts

--- holiday-booking/src/app/card/card.component.spec.ts
+++ holiday-booking/src/app/card/card.component.spec.ts
@@ -2,10 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CardComponent } from './card.component';
 
 describe('CardComponent', () => {
-  
   let component: CardComponent;
   let fixture: ComponentFixture<CardComponent>;
-
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [CardComponent]
